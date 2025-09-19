7853c8051e62af7b361646c8d6decb5b9242ad44
greendoubledecker <95518384+greendoubledecker@users.noreply.github.com>
2025-09-19 11:24:35 +0800

    Fixed the columns to rows in variable names.
    I also removed the commented code.

--- Bronze/chessboard_cses/queens.cpp
+++ Bronze/chessboard_cses/queens.cpp
@@ -7,34 +7,29 @@ using namespace std;
 vector<vector<int>> findAttacked(int x, int y){
 	vector<vector<int>> attacked;
 	for(int i = 0; i < 8; ++i){
-		//attacked.push_back(vector<int>{i, y});
-		//attacked.push_back(vector<int>{x, i});
 		attacked.push_back(vector<int>{i, x+y-i});
 		attacked.push_back(vector<int>{i, y-x+i});
-		//cout << attacked.size() << endl;
 	}
 	return attacked;
 }
 
-int recurse(int level, vector<vector<int>> reserved, vector<int> usedColumns){
+int recurse(int level, vector<vector<int>> reserved, vector<int> usedRows){
 	if(level == 8){
 		return 1;
 	}else{
 		int sum = 0;
-		//for (int i = 0; i < 8; ++i){
 		for (int j = 0; j < 8; ++j){
-			if (find(reserved.begin(), reserved.end(), vector<int>{level, j}) == reserved.end() && find(usedColumns.begin(), usedColumns.end(), j) == usedColumns.end()){
+			if (find(reserved.begin(), reserved.end(), vector<int>{level, j}) == reserved.end() && find(usedRows.begin(), usedRows.end(), j) == usedRows.end()){
 				vector<vector<int>> newRes = findAttacked(level, j);
 				vector<vector<int>> nextLayerRes;
-				vector<int> nextCol = usedColumns;
-				nextCol.push_back(j);
+				vector<int> nextRow = usedRows;
+				nextRow.push_back(j);
 				nextLayerRes.reserve(reserved.size() + newRes.size());
 				nextLayerRes.insert(nextLayerRes.end(), newRes.begin(), newRes.end());
 				nextLayerRes.insert(nextLayerRes.end(), reserved.begin(), reserved.end());
-				sum += recurse(level + 1, nextLayerRes, nextCol);
+				sum += recurse(level + 1, nextLayerRes, nextRow);
 			}
 		}
-		//}
 		return sum;
 	}
 }
