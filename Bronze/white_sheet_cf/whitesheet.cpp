2cca4287fa803a1a7d71a9045d9ceb0979174574
greendoubledecker <95518384+greendoubledecker@users.noreply.github.com>
2025-09-25 09:16:05 +0800

    White Sheet
    There is a white sheet of paper lying on a rectangle table. The sheet is a rectangle with its sides parallel to the sides of the table. If you will take a look from above and assume that the bottom left corner of the table has coordinates (0,0)
    , and coordinate axes are left and bottom sides of the table, then the bottom left corner of the white sheet has coordinates (x1,y1)
    , and the top right — (x2,y2)
    .
    
    After that two black sheets of paper are placed on the table. Sides of both black sheets are also parallel to the sides of the table. Coordinates of the bottom left corner of the first black sheet are (x3,y3)
    , and the top right — (x4,y4)
    . Coordinates of the bottom left corner of the second black sheet are (x5,y5)
    , and the top right — (x6,y6).
    
    Determine if some part of the white sheet can be seen from the above after the two black sheets are placed. The part of the white sheet can be seen if there is at least one point lying not strictly inside the white sheet and strictly outside of both black sheets.
    
    Input
    The first line of the input contains four integers x1,y1,x2,y2
     (0≤x1<x2≤106,0≤y1<y2≤106)
     — coordinates of the bottom left and the top right corners of the white sheet.
    
    The second line of the input contains four integers x3,y3,x4,y4
     (0≤x3<x4≤106,0≤y3<y4≤106)
     — coordinates of the bottom left and the top right corners of the first black sheet.
    
    The third line of the input contains four integers x5,y5,x6,y6
     (0≤x5<x6≤106,0≤y5<y6≤106)
     — coordinates of the bottom left and the top right corners of the second black sheet.
    
    The sides of each sheet of paper are parallel (perpendicular) to the coordinate axes.
    
    Output
    If some part of the white sheet can be seen from the above after the two black sheets are placed, print "YES" (without quotes). Otherwise print "NO".
    
    It took me 2.5 days to do this.
    The biggest issues were with the area calculations.

---
+++ Bronze/white_sheet_cf/whitesheet.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> overlap(int l1, int b1, int r1, int t1, int l2,int b2, int r2, int t2){
+	int left = max(l1, l2);
+	int bot = max(b1, b2);
+	int right = min(r1, r2);
+	int top = min(t1, t2);
+	//cout << "left:" << left << "bot:" << bot << "right:" << right << "top:" << top << endl;
+	vector<int> coords = {left, bot, right, top};
+	return coords;
+}
+
+long long area(int x1, int y1, int x2, int y2){
+	long long width = 1LL * (x2 - x1);
+	long long height = 1LL * (y2 - y1);
+	width = max(0LL, width);
+	height = max(0LL, height);
+	long long area = width * height;
+	return area;
+}
+
+bool whitesheet(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4, int x5, int y5, int x6, int y6){
+	vector<int> overlap1 = overlap(x1, y1, x2, y2, x3, y3, x4, y4);
+	vector<int> overlap2 = overlap(x1, y1, x2, y2, x5, y5, x6, y6);
+	vector<int> black_overlap = overlap(x3, y3, x4, y4, x5, y5, x6, y6);
+	long long area1 = area(overlap1[0], overlap1[1], overlap1[2], overlap1[3]);
+	long long area2 = area(overlap2[0], overlap2[1], overlap2[2], overlap2[3]);
+	long long doub_neg_area = area(max(x1, black_overlap[0]), max(y1, black_overlap[1]), min(x2, black_overlap[2]), min(y2, black_overlap[3]));
+	//cout << "area1: " << area1 << " area2: " << area2 << " double negative: " << doub_neg_area << endl;
+	int width = x2 - x1;// 1LL * (x2 - x1);
+	int height = y2 - y1;//1LL * (y2 - y1);
+	long long area = (long long)width * height;
+	//cout << "whitesheetarea: " << area << endl;
+	long long totalOverlap = area1 + area2 - doub_neg_area;
+	//cout << "totalOverlap: " << totalOverlap << endl;
+	if(totalOverlap < area){
+		return true;
+	}else{
+		return false;
+	}
+}
+
+int main(){
+	int x1, y1, x2, y2, x3, y3, x4, y4, x5, y5, x6, y6;
+	cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3 >> x4 >> y4 >> x5 >> y5 >> x6 >> y6;
+	if(whitesheet(x1, y1, x2, y2, x3, y3, x4, y4, x5, y5, x6, y6)){
+		cout << "YES" << endl;
+	}else{
+		cout << "NO" << endl;
+	}
+	return 0;
+}
