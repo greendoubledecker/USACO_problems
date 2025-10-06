2433533c586c38b2876e05e66d0e3f2d7b852650
greendoubledecker <95518384+greendoubledecker@users.noreply.github.com>
2025-10-06 22:02:03 +0800

    Daisy Chains
    Every day, as part of her walk around the farm, Bessie the cow visits her favorite pasture, which has N
     flowers (all colorful daisies) labeled 1…N
     lined up in a row (1≤N≤100)
    . Flower i
     has pi
     petals (1≤pi≤1000)
    .
    As a budding photographer, Bessie decides to take several photos of these flowers. In particular, for every pair of flowers (i,j)
     satisfying 1≤i≤j≤N
    , Bessie takes a photo of all flowers from flower i
     to flower j
     (including i
     and j
    ).
    
    Bessie later looks at these photos and notices that some of these photos have an "average flower" -- a flower that has P
     petals, where P
     is the exact average number of petals among all flowers in the photo.
    
    How many of Bessie's photos have an average flower?
    
    INPUT FORMAT (input arrives from the terminal / stdin):
    The first line of input contains N
    . The second line contains N
     space-separated integers p1…pN
    .
    OUTPUT FORMAT (print output to the terminal / stdout):
    Please print out the number of photos that have an average flower.
    
    It took me 1h30 to do this.
    The biggest problem was finding out the bugs in the code that counted some average photos twice

---
+++ Bronze/prob2_bronze_dec20/daisy.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+//#include <cstdlib>
+
+using namespace std;
+
+int daisies (){
+	int numflowers;
+	cin >> numflowers;
+	vector <int> flowers(numflowers);
+	for (int i = 0; i < numflowers; ++i){
+		cin >> flowers[i];
+	}
+	int avgCount = numflowers;
+	for (int i = 0; i < numflowers; ++i){
+		for (int j = i + 1; j < numflowers; ++j){
+			int sum = 0;
+			for (int k = i; k <= j; ++k){
+				sum += flowers[k];
+			}
+			cout << "i " << i << " j " << j << " sum " << sum << endl;
+			if (sum % (j-i+1) != 0){
+				continue;
+			}else{
+				int avg = sum / (j - i + 1);
+				for (int k = i; k <= j; ++k){
+					if (flowers[k] == avg){
+						++avgCount;
+						cout << avgCount << endl;
+						break;
+					}
+				}
+			}
+		}
+	}
+	return avgCount;
+}
+
+int main(){
+	cout << daisies() << endl;
+	return 0;
+}
