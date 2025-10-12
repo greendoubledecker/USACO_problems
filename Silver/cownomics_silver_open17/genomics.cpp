696120fb425b8026e51dad8f7328fdd0e9fdf53e
greendoubledecker <95518384+greendoubledecker@users.noreply.github.com>
2025-10-12 22:06:16 +0800

    Bovine Genomics
    I made it go fast. Yay.

--- Silver/cownomics_silver_open17/genomics.cpp
+++ Silver/cownomics_silver_open17/genomics.cpp
@@ -1,26 +1,24 @@
-#include <iostream>//slow. very slow.
+#include <iostream>
 #include <vector>
 #include <algorithm>
-#include <set>
+#include <unordered_set>
 #include <string>
 
 using namespace std;
 
-int genome(vector<vector<char>> spotty, vector<vector<char>> plain){ //[base][cow]
+int genome(vector<vector<int>> spotty, vector<vector<int>> plain){ //[cow][base] = base
 	int determiners = 0;
-	for(int b1 = 0; b1 < spotty.size(); ++b1){
-		for(int b2 = b1+1; b2 < spotty.size(); ++b2){
-			for(int b3 = b2+1; b3 < spotty.size(); ++b3){
-				set<string> spottyGenes; 
+	for(int b1 = 0; b1 < spotty[0].size(); ++b1){
+		for(int b2 = b1+1; b2 < spotty[0].size(); ++b2){
+			for(int b3 = b2+1; b3 < spotty[0].size(); ++b3){
+				unordered_set<int> spottyGenes; 
 				bool success = true;
-				for(int spottyCow = 0; spottyCow < spotty[0].size(); ++spottyCow){
-					vector<char> charGenes = {spotty[b1][spottyCow], spotty[b2][spottyCow], spotty[b3][spottyCow]};
-					string genes(charGenes.begin(), charGenes.end());
+				for(int spottyCow = 0; spottyCow < spotty.size(); ++spottyCow){
+					int genes = (spotty[spottyCow][b1] << 4) + (spotty[spottyCow][b2] << 2) + spotty[spottyCow][b3];
 					spottyGenes.insert(genes);
 				}
-				for(int plainCow = 0; plainCow < plain[0].size(); ++plainCow){
-					vector<char> charGenes = {plain[b1][plainCow], plain[b2][plainCow], plain[b3][plainCow]};
-					string genes(charGenes.begin(), charGenes.end());
+				for(int plainCow = 0; plainCow < plain.size(); ++plainCow){
+					int genes = (plain[plainCow][b1] << 4) + (plain[plainCow][b2] << 2) + plain[plainCow][b3];
 					if(spottyGenes.count(genes)){
 						success = false;
 						break;
@@ -35,17 +33,32 @@ int genome(vector<vector<char>> spotty, vector<vector<char>> plain){ //[base][co
 	return determiners;
 }
 
+int convert(char letter){
+	if(letter == 'A'){
+		return 0;
+	}
+	else if(letter == 'T'){
+		return 1;
+	}
+	else if(letter == 'C'){
+		return 2;
+	}
+	else {
+		return 3;
+	}
+}
+
 int main(){
 	int n, m;
 	cin >> n >> m;
-	vector<vector<char>>spotty(m, vector<char>(n));
-	vector<vector<char>> plain(m, vector<char>(n));
+	vector<vector<int>> spotty(n, vector<int>(m));
+	vector<vector<int>> plain(n, vector<int>(m));
 	for(int i = 0; i < n; ++i){
 		//vector<char> cow;
 		for(int j = 0; j < m; ++j){
 			char base;
 			cin >> base;
-			spotty[j][i] = base;
+			spotty[i][j] = convert(base);
 		}
 	}
 	for(int i = 0; i < n; ++i){
@@ -53,7 +66,7 @@ int main(){
 		for(int j = 0; j < m; ++j){
 			char base;
 			cin >> base;
-			plain[j][i] = base;
+			plain[i][j] = convert(base);
 		}
 	}
 	cout << genome(spotty, plain) << endl;
