ea189681df17c8a3c41b22a4d5aed2503508f2de
greendoubledecker <95518384+greendoubledecker@users.noreply.github.com>
2025-10-13 11:16:53 +0800

    Mex vs Med
    You are given a permutation p1,p2,…,pn
     of length n
     of numbers 0,…,n−1
    . Count the number of subsegments 1≤l≤r≤n
     of this permutation such that mex(pl,pl+1,…,pr)>med(pl,pl+1,…,pr)
    .
    
    mex
     of S
     is the smallest non-negative integer that does not occur in S
    . For example:
    
    mex(0,1,2,3)=4
    mex(0,4,1,3)=2
    mex(5,4,0,1,2)=3
    med
     of the set S
     is the median of the set, i.e. the element that, after sorting the elements in non-decreasing order, will be at position number ⌊(|S|+1)/2⌋
     (array elements are numbered starting from 1
     and here ⌊v⌋
     denotes rounding v
     down.). For example:
    
    med(0,1,2,3)=1
    med(0,4,1,3)=1
    med(5,4,0,1,2)=2
    A sequence of n
     numbers is called a permutation if it contains all the numbers from 0
     to n−1
     exactly once.
    
    Input
    The first line of the input contains a single integer t
     (1≤t≤10^4
    ), the number of test cases.
    
    The descriptions of the test cases follow.
    
    The first line of each test case contains a single integer n
     (1≤n≤2⋅10^5
    ), the length of the permutation p
    .
    
    The second line of each test case contains exactly n
     integers: p1,p2,…,pn
     (0≤pi≤n−1
    ), elements of permutation p
    .
    
    It is guaranteed that the sum of n
     over all test cases does not exceed 2⋅105
    .
    
    Output
    For each test case print the answer in a single line: the number of subsegments 1≤l≤r≤n
     of this permutation such that mex(pl,pl+1,…,pr)>med(pl,pl+1,…,pr)
    .

---
+++ Bronze/mex_med_cf/mexmed.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void tc(){
+	int num;
+	cin >> num;
+	vector<int> permutation;
+	for(int i = 0; i < num; ++i){
+		int value;
+		cin >> value;
+		permutation.push_back(value);
+	}
+	int ans = 0;
+	for(int l = 0; l < num; ++l){
+		for(int r = l + 1; r <= num; ++r){
+			vector<int> subset(permutation.begin() + l, permutation.begin() + r);
+			sort(subset.begin(), subset.end());
+			bool success = true;
+			double middle = (r - l)/2.0;
+			for(int i = 0; i < middle; ++i){
+				//cout << "x" << endl;
+				if(subset[i] != i){
+					success = false;
+					break;
+				}
+			}
+			//cout << "Subset: ";
+			//cout << endl << "success = " << success << endl;
+			if(success){
+				ans++;
+			}
+		}
+	}
+	cout << ans << endl;
+}
+
+int main(){
+	int tests;
+	cin >> tests;
+	for(int i = 0; i < tests; ++i){
+		tc();
+	}
+	return 0;
+}
