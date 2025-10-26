09d9e238fdb1ebabf68e5acc53c0d9671f920795
greendoubledecker <95518384+greendoubledecker@users.noreply.github.com>
2025-10-26 15:39:13 +0200

    Twenty Four
    Twenty-four is a popular card game designed to be played by four players. Each player is dealt a deck of cards, which are kept face down. On every turn, each of the four players turns over the top card of his or her deck, so that it is visible to all. The goal is to find an arithmetic expression using the values of the cards (with A meaning 1, J meaning 11, Q meaning 12, and K meaning 13) that evaluates to the number 24. For example, for the example in the illustration, one possible expression would be:
    ((A * K) - J) * Q
    ((1*13) -11)*12
    
    The first player to find such an expression wins the turn, and adds all four cards to the bottom of his or her deck.
    
    Each valid arithmetic expression must use all four cards, and must combine their values using addition, subtraction, multiplication, or division. Parentheses are allowed to specify precedence of operations. Juxtaposing cards to make multiple-digit decimal numbers is not allowed (e.g. you cannot place the cards 2 and 4 beside each other to make 24).
    
    Non-integer quotients of division are also not allowed, even as a partial result (of a subexpression of the overall expression).
    
    In some cases, the players may take a very long time to find an expression evaluating to 24. In fact, in some cases, no such expression exists. Your task is to determine, given four cards, an expression that evaluates to the largest number less than or equal to 24.
    
    Input Specification
    The first line contains an integer 1 <= N <= 5
     indicating the number of card hands that follow. Each hand consists of four lines. Each of these lines is an integer
     indicating the value of a card.
    
    Output Specification
    For each hand, output a line containing an integer n
     if the cards can be combined using arithmetic operators to evaluate to n
    . The value n
     should be the largest possible value amongst all possible arithmetic expressions using these 4 cards, so long as n <= 24.
    
    This took me 4hrs to do.
    The biggest problem was getting the helper functions to work.

---
+++ Bronze/24_ccc/24.cpp
@@ -0,0 +1,122 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
+int cards = 4;
+
+bool checkIfInt(int num, int den){
+	if(den == 0){
+		return false;
+	}
+	return num % den == 0;
+}
+
+int add(int a, int b, vector<int> nums);
+int sub(int a, int b, vector<int> nums);
+int mul(int a, int b, vector<int> nums);
+int div(int a, int b, vector<int> nums);
+
+int recurse(vector<int> nums){
+	if(nums.size() == 1){
+		if(nums[0] <= 24){
+			return nums[0];
+		}
+		return INT_MIN;
+	}
+	int maxAns = INT_MIN;
+	for(int a = 0; a < nums.size(); ++a){
+		for(int b = a + 1; b < nums.size(); ++b){
+			int curr = max({
+			add(a, b, nums),
+			sub(a, b, nums),
+			mul(a, b, nums),
+			div(a, b, nums)
+			});
+			if(curr > maxAns){
+				maxAns = curr;
+			}
+		}
+	}
+	return maxAns;
+}
+
+int add(int a, int b, vector<int> nums){
+	int temp = a;
+	a = max(a, b);
+	b = min(temp, b);
+	vector<int> res = nums;
+	res.erase(res.begin() + a);
+	res.erase(res.begin() + b);
+	res.push_back(nums[a] + nums[b]);
+	return recurse(res);
+}
+int sub(int a, int b, vector<int> nums){
+	int temp = a;
+	a = max(a, b);
+	b = min(temp, b);
+	vector<int> res = nums;
+	int ASubB, BSubA;
+	res.erase(res.begin() + a);
+	res.erase(res.begin() + b);
+	res.push_back(nums[a] - nums[b]);
+	ASubB = recurse(res);
+	res.pop_back();
+	res.push_back(nums[b] - nums[a]);
+	BSubA = recurse(res);
+	return max(ASubB, BSubA);
+}
+int mul(int a, int b, vector<int> nums){
+	int temp = a;
+	a = max(a, b);
+	b = min(temp, b);
+	vector<int> res = nums;
+	res.erase(res.begin() + a);
+	res.erase(res.begin() + b);
+	res.push_back(nums[a] * nums[b]);
+	return recurse(res);
+}
+int div(int a, int b, vector<int> nums){
+	int temp = a;
+	a = max(a, b);
+	b = min(temp, b);
+	vector<int> res = nums;
+	int ADivB, BDivA;
+	res.erase(res.begin() + a);
+	res.erase(res.begin() + b);
+	if(checkIfInt(nums[a], nums[b])){
+		res.push_back(nums[a] / nums[b]);
+		ADivB = recurse(res);
+		res.pop_back();
+	}else{
+		ADivB = INT_MIN;
+	}
+	if(checkIfInt(nums[b], nums[a])){
+		res.push_back(nums[b] / nums[a]);
+		BDivA = recurse(res);
+	}else{
+		BDivA = INT_MIN;
+	}
+	return max(ADivB, BDivA);
+}
+
+void tc(){
+	vector<int> hand;
+	for(int i = 0; i < cards; ++i){
+		int card;
+		cin >> card;
+		hand.push_back(card);
+	}
+	cout << recurse(hand) << endl;
+}
+
+int main(){
+	int hands;
+	cin >> hands;
+	for(int i = 0; i < hands; ++i){
+		tc();
+	}
+	return 0;
+}
