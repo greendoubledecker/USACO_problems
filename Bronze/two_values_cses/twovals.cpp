a6d3f3c843f0e5c36bbb55d6aed29c4f46e009a6
greendoubledecker <95518384+greendoubledecker@users.noreply.github.com>
2025-10-29 21:21:10 +0200

    Sum of Two Values
    You are given an array of n integers, and your task is to find two values (at distinct positions) whose sum is x.
    Input
    The first input line has two integers n and x: the array size and the target sum.
    The second line has n integers a_1,a_2, ... ,a_n: the array values.
    Output
    Print two integers: the positions of the values. If there are several solutions, you may print any of them. If there are no solutions, print IMPOSSIBLE.
    
    It took me ~4 hours to do this.
    The biggest problem was getting the if conditionals to work.

---
+++ Bronze/two_values_cses/twovals.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+int main(){
+	int n, tgt;
+	cin >> n >> tgt;
+	unordered_map<int, vector<int>> vec;
+	for(int i = 0; i < n; ++i){
+		int e;
+		cin >> e;
+		if(e < tgt){
+			vec[e].push_back(i);
+		}
+	}
+	for(const auto& pair : vec){
+		int remaining = tgt - pair.first;
+		if(pair.second == vector<int>{}){
+			continue;
+		}
+		if(vec[remaining] != vector<int>{}){
+			if(pair.first == remaining){
+				if(pair.second.size() >= 2){
+					cout << pair.second[0] + 1 << " " << pair.second[1] + 1 << endl;
+					return 0;
+				}
+			}else{
+				cout << pair.second[0] + 1 << " " << vec[remaining][0] + 1 << endl;
+				return 0;
+			}
+		}
+	}
+	cout << "IMPOSSIBLE" << endl;
+	return 0;
+}
