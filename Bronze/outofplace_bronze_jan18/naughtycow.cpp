381e3ec6b19cd5f95531e98996c1f16683038355
greendoubledecker <95518384+greendoubledecker@users.noreply.github.com>
2025-11-10 11:41:19 +0800

    Out of Place
    Feeling ambitious, Farmer John plans to attempt something that never seems to go quite right: he wants to take a photograph of his entire herd of cows.
    To make the photograph look nice, he wants the cows to line up in a single row from shortest to tallest. Unfortunately, right after he has the cows line up this way, Bessie the cow, always the troublemaker, steps out of line and re-inserts herself at some other location in the lineup!
    
    Farmer John would like to swap pairs of cows so the entire herd is again lined up properly. Please help him determine the minimum number of swaps he needs to make between pairs of cows in order to achieve this goal.
    
    INPUT FORMAT (file outofplace.in):
    The first line of input contains N (2≤N≤100). The next N lines describe the heights of the cows as they are lined up after Bessie makes her move. Each cow height is an integer in the range 1…1,000,000. Cows may have the same height.
    OUTPUT FORMAT (file outofplace.out):
    Please output the minimum number of times Farmer John needs to swap pairs of cows in order to achieve a proper ordering. Swaps do not necessarily need to involve adjacent cows in the ordering.
    
    It took me 30 - 45 mins to do this problem.
    The only issue was that I forgot to subtract 1 from ans.

---
+++ Bronze/outofplace_bronze_jan18/naughtycow.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+	freopen("outofplace.in", "r", stdin);
+	freopen("outofplace.out", "w", stdout);
+	vector<int> current, correct;
+	int num;
+	cin >> num;
+	for(int i = 0; i < num; ++i){
+		int cow;
+		cin >> cow;
+		current.push_back(cow);
+	}
+	correct = current;
+	sort(correct.begin(), correct.end());
+	int ans = 0;
+	for(int i = 0; i < num; ++i){
+		if(current[i] != correct[i]){
+			++ans;
+		}
+	}
+	cout << ans - 1 << endl;
+	return 0;
+}
