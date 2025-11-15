6231684efe5e990fcaf521e0383a5a3dda5234f3
greendoubledecker <95518384+greendoubledecker@users.noreply.github.com>
2025-11-15 19:52:54 +0800

    Got it fixed!(Angry Cows)
    USACO 2016 January Contest, Bronze
    Problem 2. Angry Cows
    Bessie the cow has designed what she thinks will be the next big hit video game: "Angry Cows". The premise, which she believes is completely original, is that the player shoots a cow with a slingshot into a one-dimensional scene consisting of a set of hay bales located at various points on a number line; the cow lands on a hay bale with sufficient force to cause the bale to explode, which in turn might set of a chain reaction that causes additional nearby hay bales to explode. The goal is to use a single cow to start a chain reaction that detonates as many hay bales as possible.
    There are N hay bales located at distinct integer positions x1,x2,…,xN on the number line. If a cow is launched onto a hay bale at position x, this hay bale explodes with a "blast radius" of 1, meaning that any other hay bales within 1 unit of distance are also engulfed by the explosion. These neighboring bales then themselves explode (all simultaneously), each with a blast radius of 2, so these explosions may engulf additional yet-unexploded bales up to 2 units of distance away. In the next time step, these bales also explode (all simultaneously) with blast radius 3. In general, at time t a set of hay bales will explode, each with blast radius t. Bales engulfed by these explosions will themselves explode at time t+1 with blast radius t+1, and so on.
    
    Please determine the maximum number of hay bales that can explode if a single cow is launched onto the best possible hay bale to start a chain reaction.
    
    INPUT FORMAT (file angry.in):
    The first line of input contains N (1≤N≤100). The remaining N lines all contain integers x1…xN (each in the range 0…1,000,000,000).
    OUTPUT FORMAT (file angry.out):
    Please output the maximum number of hay bales that a single cow can cause to explode.
    SAMPLE INPUT:
    6
    8
    5
    6
    13
    3
    4
    SAMPLE OUTPUT:
    5
    In this example, launching a cow onto the hay bale at position 5 will cause the bales at positions 4 and 6 to explode, each with blast radius 2. These explosions in turn cause the bales at positions 3 and 8 to explode, each with blast radius 3. However, these final explosions are not strong enough to reach the bale at position 13.
    
    Problem credits: Brian Dean
    
    It took me ages to debug that switch from line 24 to line 20...

--- Bronze/angry_bronze_jan16/angry.cpp
+++ Bronze/angry_bronze_jan16/angry.cpp
@@ -5,8 +5,6 @@
 using namespace std;
 
 int main(){
-	//freopen("angry.in", "r", stdin);   // Redirect cin to read from file
-    //freopen("angry.out", "w", stdout); // Redirect cout to write to file
 	int num;
 	cin >> num;
 	vector<int> bales(num);
@@ -19,12 +17,10 @@ int main(){
 	int max = 0;
 	for(int pos : bales){
 		left = pos; right = pos;
+		finLeft = -1; finRight = -1;
 		int t = 1;
 		while (true){
-			finLeft = -1; finRight = -1;
 			prevLeft = left; prevRight = right;
-			//shrink range and count bales 💥'ed
-			// if there's a mess of characters it was a explode emoj
 			if(finLeft < 0){
 				left -= t;
 				for(int i = left; i <= prevLeft; ++i){
@@ -43,13 +39,10 @@ int main(){
 					}
 				}
 			}
-			cout << "range " << finLeft << " to " << finRight << endl;
 			if(left == prevLeft && finLeft == -1){
-				cout << "stop left" << endl;
 				finLeft = left;
 			}
 			if(right == prevRight && finRight == -1){
-				cout << "stop right" << endl;
 				finRight = right;
 			}
 			if(finRight >= 0 && finLeft >= 0){
@@ -57,13 +50,11 @@ int main(){
 			}
 			++t;
 		}
-		cout << "range " << left << " to " << right << endl;
 		auto start = lower_bound(bales.begin(), bales.end(), finLeft);
 		auto end = upper_bound(bales.begin(), bales.end(), finRight);
 		if(end - start > max){
 			max = end - start;
 		}
-		cout << "pos " << pos << " exploded " << end - start << " bales" << endl;
 	}
 	cout << max << endl;
 	return 0;
