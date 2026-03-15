2e609e4eab6dcb0ca02705cfaf6b6c11fc5bb310
greendoubledecker <95518384+greendoubledecker@users.noreply.github.com>
2026-03-15 16:10:01 +0800

    Sum Of Three Values
    https://cses.fi/problemset/task/1641/

---
+++ Silver/3sum_cses/3sum.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+bool skedfj(pair<ll, ll> a, pair<ll, ll> b){
+	return a.first < b.first;
+}
+pair<ll, ll> twosum(vector<pair<ll, ll>> v, ll x){
+	//cout << "x = " << x << endl;
+	ll l = 0, r = v.size() - 1;
+	while(l < r){
+		if(v[l].first + v[r].first > x){
+			--r;
+		}else if(v[l].first + v[r].first < x){
+			++l;
+		}else{
+			return {v[l].second, v[r].second};
+		}
+	}
+	return {-1, -1};
+}
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	ll n, x;
+	cin >> n >> x;
+	vector<pair<ll, ll>> v(n);
+	for(ll i = 0; i < n; ++i){
+		ll w;
+		cin >> w;
+		v[i] = {w, i};
+	}
+	sort(v.begin(), v.end(), skedfj);
+	for(ll i = 0; i < n; ++i){
+		vector<pair<ll, ll>> v2 = v;
+		v2.erase(v2.begin() + i);
+		pair<ll, ll> p = twosum(v2, x - v[i].first);
+		if(p.first != -1){
+			cout << v[i].second + 1 << ' ' << p.first + 1 << ' ' << p.second + 1 << '\n';
+			return 0;
+		}
+	}
+	cout << "IMPOSSIBLE\n";
+}
