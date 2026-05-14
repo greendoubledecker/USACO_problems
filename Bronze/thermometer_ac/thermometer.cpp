f857d57d9aea01068341fd7b91af334e4ae4e48d
greendoubledecker <95518384+greendoubledecker@users.noreply.github.com>
2026-05-14 21:17:19 +0800

    Easy question
    Atcoder ABC 397A

---
+++ Bronze/thermometer_ac/thermometer.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+using D = double;
+int main(){
+	D d;
+	cin >> d;
+	if(d >= 38){
+		cout << "1\n";
+	}
+	else if(d >= 37.5){
+		cout << "2\n";
+	}else{
+		cout << "3\n";
+	}
+}
